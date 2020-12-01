968dd0f0466b1bebb6f0762de7b1cd9a94400c68
Greg Kapka <greg@oraclize.it>
2020-12-01 10:00:56 +0000

    feat(price-getter): <- adds exchange rate getter to that

--- src/lib/get_price.rs
+++ src/lib/get_price.rs
@@ -1,5 +1,5 @@
-use crate::lib::{assets::Assets, types::Result};
+use crate::lib::{assets::Assets, types::Result, rates::ExchangeRates};
 
 pub fn get_price_of(assets: &[String], amounts: &[f64]) -> Result<String> {
-    Ok(Assets::from_strings(assets)?.get_prices_json(amounts)?.to_string())
+    Ok(Assets::from_strings(assets)?.get_prices_json(amounts, &ExchangeRates::get()?)?.to_string())
 }
